fc4cfb3fe4be9a10065a8bdca740c0e2beead1ea
Dan Wang <danwang625@gmail.com>
2024-02-12 21:11:18 -0800

    add server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use('/', (req, res, next) => {
+    res.send('hi, therer')
+    next()
+})
+
+app.listen(port, () => {
+    console.log(`app is listening on port: ${port}`)
+})
