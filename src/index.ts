1730b8dba0bcdc5f466bd2bd4933d9309a988338
Dan Wang <danwang625@gmail.com>
2024-02-19 23:48:22 -0800

    update schema

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from "express"
 import { initMongoose } from "./db"
 import router from './router'
 import bodyParser from "body-parser"
+import cors from 'cors'
 
 
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/', router)
 
